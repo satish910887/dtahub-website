bbd25fbe264df3b83efc977909a2ba163fb59e72
Satish <satish910887@gmail.com>
2026-09-23 23:38:35 +0530

    Fix package.json + ws package

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -6,7 +6,10 @@ export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY,
   {
-    auth: { persistSession: false }
+    auth: { persistSession: false, autoRefreshToken: false },
+    realtime: {
+      params: { eventsPerSecond: 0 }
+    }
   }
 );
 
